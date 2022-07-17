30de4aa4aafbd7f1506f20fa2387df79477ecfd1
Tate Smith <thsmith.cofc@gmail.com>
2022-07-17 19:07:49 +0200

    deploy prework

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int yearBorn = 1990;
+    String monthBorn = "June";
+    boolean weekend = true;
+
+    System.out.println(yearBorn);
+    System.out.println(monthBorn);
+    System.out.println(weekend);
+  }
+}
