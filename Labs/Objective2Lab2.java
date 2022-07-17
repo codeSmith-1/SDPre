a688342b773a37e913ba2862b683fd43fb3868c7
Tate Smith <thsmith.cofc@gmail.com>
2022-07-17 18:59:02 +0200

    deploy prework

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Tate Smith";
+    int age = 32;
+    String state = "Florida";
+    System.out.println("Name: "+name+" Age: "+age+" State: "+state);
+  }
+}
